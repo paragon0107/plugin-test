41e4e3ea51d769bd0458b7e75375f04734d26aaa
smg0725 <smg0725@naver.com>
2025-05-15 07:25:10 +0900

    qwe

--- plugin/code.ts
+++ plugin/code.ts
@@ -1,6 +1,6 @@
 declare const SITE_URL: string;
 
-figma.showUI(`<script>window.location.href = '${loca}'</script>`, {
+figma.showUI(`<script>window.location.href = '${SITE_URL}'</script>`, {
   width: 700,
   height: 700,
 });
